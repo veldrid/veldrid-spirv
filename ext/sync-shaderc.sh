518a9d2b012b3f872a3d9883f0322e8359db39bb
Eric Mellino <mellinoe@gmail.com>
2018-06-21 22:10:48 -0700

    Fix sync-shaders.sh and CI config files.

--- ext/sync-shaderc.sh
+++ ext/sync-shaderc.sh
@@ -2,4 +2,4 @@
 
 scripthPath="`dirname \"$0\"`"
 
-py $scripthPath/update_shaderc_sources.py --dir $scripthPath/shaderc --file $scripthPath/known_good.json
+python $scripthPath/update_shaderc_sources.py --dir $scripthPath/shaderc --file $scripthPath/known_good.json
