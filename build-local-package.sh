b0d26299cf004882554bb88689bd367d5c87396e
Eric Mellino <mellinoe@gmail.com>
2018-06-23 15:12:39 -0700

    Allow build-local-package.sh to select Debug/Release config.

--- build-local-package.sh
+++ build-local-package.sh
@@ -2,5 +2,27 @@
 
 scriptPath="`dirname \"$0\"`"
 
-$scriptPath/build-native.sh Release
-dotnet pack -c Release $scriptPath/src/Veldrid.SPIRV/Veldrid.SPIRV.csproj
+_BuildConfig = Debug
+
+while :; do
+    if [ $# -le 0 ]; then
+        break
+    fi
+
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        debug|-debug)
+            _BuildConfig=Debug
+            ;;
+        release|-release)
+            _BuildConfig=Release
+            ;;
+        *)
+            __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
+    esac
+
+    shift
+done
+
+$scriptPath/build-native.sh $_BuildConfig
+dotnet pack -c $_BuildConfig $scriptPath/src/Veldrid.SPIRV/Veldrid.SPIRV.csproj
