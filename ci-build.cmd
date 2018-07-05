19bcf4f2c4e3d834adfcac093a1a336b7e2a7245
Eric Mellino <mellinoe@gmail.com>
2018-07-04 22:25:54 -0700

    Change binary names so that AppVeyor can find them.

--- ci-build.cmd
+++ ci-build.cmd
@@ -3,4 +3,6 @@
 
 call %~dp0ext/sync-shaderc.cmd
 call %~dp0build-native.cmd Release x64
+copy %~dp0build\x64\Release\libveldrid-spirv.dll %~dp0build\x64\Release\libveldrid-spirv.win-x64.dll
 call %~dp0build-native.cmd Release x86
+copy %~dp0build\x86\Release\libveldrid-spirv.dll %~dp0build\x86\Release\libveldrid-spirv.win-x86.dll
