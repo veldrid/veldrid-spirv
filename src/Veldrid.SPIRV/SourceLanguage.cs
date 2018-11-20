138828f5ab59f491ff88428216fa6600ae76f35d
Chance Snow <git@chancesnow.me>
2018-11-20 09:46:38 -0800

    Add doc comments to SourceLanguage enum members

--- src/Veldrid.SPIRV/SourceLanguage.cs
+++ src/Veldrid.SPIRV/SourceLanguage.cs
@@ -3,7 +3,13 @@ namespace Veldrid.SPIRV
     /// <summary>Source language from which to compile a shader.</summary>
     public enum SourceLanguage
     {
+        /// <summary>
+        /// Compile a shader from GLSL source.
+        /// </summary>
         GLSL,
+        /// <summary>
+        /// Compile a shader from HLSL source.
+        /// </summary>
         HLSL,
     }
 }
