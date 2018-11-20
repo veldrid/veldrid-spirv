9116d550916eb06722da8ba425e5d09e2ca41b93
Chance Snow <git@chancesnow.me>
2018-11-20 08:57:40 -0800

    Fix SourceLanguage enum visibility

--- src/Veldrid.SPIRV/SourceLanguage.cs
+++ src/Veldrid.SPIRV/SourceLanguage.cs
@@ -1,7 +1,7 @@
 namespace Veldrid.SPIRV
 {
     /// <summary>Source language from which to compile a shader.</summary>
-    enum SourceLanguage
+    public enum SourceLanguage
     {
         GLSL,
         HLSL,
