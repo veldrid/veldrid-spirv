88e365765b256936a579f83583aca092ccb0a78a
Eric Mellino <mellinoe@gmail.com>
2018-12-10 21:39:17 -0800

    Attempt to get Android dynamic libraries building in AppVeyor.

--- build-native.cmd
+++ build-native.cmd
@@ -1,39 +1,51 @@
 @setlocal
 @echo off
 
-set BUILD_CONFIG=Debug
-set BUILD_ARCH=x64
-set BUILD_CMAKE_GENERATOR_PLATFORM=x64
+set _CMAKE_BUILD_TYPE=Debug
+set _BUILD_ARCH=
+set _CMAKE_GENERATOR_PLATFORM=x64
+set _NDK_DIR=
+set _ANDROID_ABI=arm64-v8a
+set _OS_DIR=
 
 :ArgLoop
 if [%1] == [] goto LocateVS
-if /i [%1] == [Release] (set BUILD_CONFIG=Release&& shift & goto ArgLoop)
-if /i [%1] == [Debug] (set BUILD_CONFIG=Debug&& shift & goto ArgLoop)
-if /i [%1] == [x64] (set BUILD_ARCH=x64&& shift & goto ArgLoop)
-if /i [%1] == [x86] (set BUILD_ARCH=x86&& set BUILD_CMAKE_GENERATOR_PLATFORM=Win32&&shift & goto ArgLoop)
+if /i [%1] == [Release] (set _CMAKE_BUILD_TYPE=Release&& shift & goto ArgLoop)
+if /i [%1] == [Debug] (set _CMAKE_BUILD_TYPE=Debug&& shift & goto ArgLoop)
+if /i [%1] == [win-x64] (set _BUILD_ARCH=x64&& set _CMAKE_GENERATOR_PLATFORM=x64&& shift & goto ArgLoop)
+if /i [%1] == [win-x86] (set _BUILD_ARCH=x86&& set _CMAKE_GENERATOR_PLATFORM=Win32&& shift & goto ArgLoop)
+if /i [%1] == [--android-ndk] (set _NDK_DIR=%2&& shift && shift & goto ArgLoop)
+if /i [%1] == [--android-abi] (set _ANDROID_ABI=%2&& set _BUILD_ARCH=%2&& shift && shift & goto ArgLoop)
 shift
 goto ArgLoop
 
 :LocateVS
-set _VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-if exist %_VSWHERE% (
-  for /f "usebackq tokens=*" %%i in (`%_VSWHERE% -latest -property installationPath`) do set _VSCOMNTOOLS=%%i\Common7\Tools
-)
-if not exist "%_VSCOMNTOOLS%" set _VSCOMNTOOLS=%VS140COMNTOOLS%
-if not exist "%_VSCOMNTOOLS%" goto :NoVisualStudio
 
-call "%_VSCOMNTOOLS%\VsDevCmd.bat"
+set _CMAKE_ARGS=-DCMAKE_BUILD_TYPE=%_CMAKE_BUILD_TYPE%
+if defined _NDK_DIR (
+  set _CMAKE_ARGS=%_CMAKE_ARGS% -G "MinGW Makefiles" -DCMAKE_MAKE_PROGRAM="%_NDK_DIR%\prebuilt\windows-x86_64\bin\make.exe" -DCMAKE_TOOLCHAIN_FILE="%_NDK_DIR%\build\cmake\android.toolchain.cmake"
+  set _OS_DIR=android
 
-If NOT exist ".\build\%BUILD_ARCH%" (
-  mkdir build\%BUILD_ARCH%
+  if not defined _BUILD_ARCH (
+    set _BUILD_ARCH=%_ANDROID_ABI%
+  )
+
+) else (
+  set _CMAKE_ARGS=%_CMAKE_ARGS% -DCMAKE_GENERATOR_PLATFORM=%_CMAKE_GENERATOR_PLATFORM%
+  set _OS_DIR=win
 )
-pushd build\%BUILD_ARCH%
-cmake -DCMAKE_GENERATOR_PLATFORM=%BUILD_CMAKE_GENERATOR_PLATFORM% ..\..
+
+set _CMAKE_ARGS=%_CMAKE_ARGS% ..\..
+
+set _BUILD_DIR=.\build\%_OS_DIR%-%_BUILD_ARCH%
+
+If NOT exist "%BUILD_DIR%" (
+  mkdir %_BUILD_DIR%
+)
+pushd %_BUILD_DIR%
+cmake %_CMAKE_ARGS%
+cmake --build .
 popd
-
-echo Calling msbuild build\%BUILD_ARCH%\ALL_BUILD.vcxproj /p:Configuration=%BUILD_CONFIG%
-
-msbuild build\%BUILD_ARCH%\ALL_BUILD.vcxproj /p:Configuration=%BUILD_CONFIG%
 
 :Success
 exit /b 0
