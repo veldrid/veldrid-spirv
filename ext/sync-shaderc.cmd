bf79f6c86fa27b8da19f953e2ec048ed2601aa63
Eric Mellino <mellinoe@gmail.com>
2021-02-16 19:39:12 -0800

    Update external projects.

--- ext/sync-shaderc.cmd
+++ ext/sync-shaderc.cmd
@@ -1,4 +1,4 @@
 @setlocal
 @echo off
 
-py %~dp0update_shaderc_sources.py --dir %~dp0shaderc --file %~dp0known_good.json
+python %~dp0update_shaderc_sources.py --dir %~dp0shaderc --file %~dp0known_good.json
