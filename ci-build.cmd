1e2af6c1f807ee3fbf8df2d7654a52879ee8709c
Martyn York <etinquis@gmail.com>
2022-01-03 20:36:14 -0800

    compile for x86_64

--- ci-build.cmd
+++ ci-build.cmd
@@ -9,4 +9,5 @@ call %~dp0ext/sync-shaderc.cmd
 call %~dp0build-native.cmd Release win-x64
 call %~dp0build-native.cmd Release win-x86
 call %~dp0build-native.cmd Release --android-ndk "%_ANDROID_NDK%" --android-abi arm64-v8a
+call %~dp0build-native.cmd Release --android-ndk "%_ANDROID_NDK%" --android-abi x86_64
 call %~dp0build-native.cmd Release --android-ndk "%_ANDROID_NDK%" --android-abi armeabi-v7a
