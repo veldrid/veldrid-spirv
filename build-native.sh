b7816442c648f072d94b3b7fb15dca02b26d52dd
Salman Ahmed <frenzibyte@gmail.com>
2022-06-03 06:14:04 +0300

    Standardise native build output directory and improve CI workflow (#25)
    * Update native building sh script with OS directory
    
    * Update native building cmd script with OS directory
    
    * Update projects inline with path changes
    
    * Change android platform specification to match others
    
    * Add conditional for "copy to outside folder" operation
    
    Turns out only Windows has this effect (dir: `build/Release/win-x64`, output: `build/Release/win-x64/Release`). Android is unaffected.
    
    * Update local package scripts
    
    * Attempt to improve packaging logic
    
    * Move tests step per platform for better coverage
    
    * Include project building as part of NuGet packaging
    
    Was causing `runtimes` folder to not be included in the package.
    
    * Force checkout depth to 0 on the matrix
    
    Unsure if this is good or bad.
    
    * Only perform per-platform tests for 64-bit architectures

--- build-native.sh
+++ build-native.sh
@@ -10,6 +10,7 @@ _CMakeBuildTarget=veldrid-spirv
 _CMakeOsxArchitectures=
 _CMakeGenerator=
 _CMakeExtraBuildArgs=
+_OSDir=
 
 while :; do
     if [ $# -le 0 ]; then
@@ -24,18 +25,22 @@ while :; do
         release|-release)
             _CMakeBuildType=Release
             ;;
+        osx)
+            _CMakeOsxArchitectures=$2
+            _OSDir=osx
+            shift
+            ;;
+        linux-x64)
+            _OSDir=linux-x64
+            ;;
         ios)
             _CMakeToolchain=-DCMAKE_TOOLCHAIN_FILE=$scriptPath/ios/ios.toolchain.cmake
             _CMakePlatform=-DPLATFORM=OS64COMBINED
             _CMakeEnableBitcode=-DENABLE_BITCODE=0
-            _OutputPathPrefix=ios-
             _CMakeBuildTarget=veldrid-spirv-combined_genfile
             _CMakeGenerator="-G Xcode -T buildsystem=1"
             _CMakeExtraBuildArgs="--config Release"
-            ;;
-        -osx-architectures)
-            _CMakeOsxArchitectures=$2
-            shift
+            _OSDir=ios
             ;;
         *)
             __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
@@ -44,7 +49,7 @@ while :; do
     shift
 done
 
-_OutputPath=$scriptPath/build/$_OutputPathPrefix$_CMakeBuildType
+_OutputPath=$scriptPath/build/$_CMakeBuildType/$_OSDir
 _PythonExePath=$(which python3)
 if [[ $_PythonExePath == "" ]]; then
     echo Build failed: could not locate python executable.
@@ -53,6 +58,10 @@ fi
 
 mkdir -p $_OutputPath
 pushd $_OutputPath
-cmake ../.. -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator $_CMakeToolchain $_CMakePlatform $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures"
+cmake ../../.. -DCMAKE_BUILD_TYPE=$_CMakeBuildType $_CMakeGenerator $_CMakeToolchain $_CMakePlatform $_CMakeEnableBitcode -DPYTHON_EXECUTABLE=$_PythonExePath -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures"
 cmake --build . --target $_CMakeBuildTarget $_CMakeExtraBuildArgs
+
+if [[ $_OSDir == "ios" ]]; then
+    cp ./$_CMakeBuildType-*/* ./
+fi
 popd
